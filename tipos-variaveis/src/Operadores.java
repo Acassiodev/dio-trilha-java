6637efecd49bfe3bbcb7b2b656641fcc03258a9c
Acassiodev <acassio_15_@hotmail.com>
2024-04-27 16:28:02 -0300

    atual

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,11 +1,23 @@
 public class Operadores {
     public static void main(String[] args) {
-        int numero = 5;
-        //x repetição 
-        numero = numero + 2;
+        String nomeUm = "ACASSIO";
+        String nomeDois = "ACASSIO";
 
-        System.out.println(numero);
 
+        System.out.println(nomeUm == nomeDois);
+        
+
+
+        int numero1 = 1;
+        int numnero2 = 2;
+
+        boolean simNao = numero1 == numnero2;
+
+        System.out.println("numeroUm é igual a numeroDois?" + simNao);
+
+        simNao = numero1 > numnero2;
+
+        System.out.println("numeroUm é maior que numeroDois?" + simNao);
 
     }
 
